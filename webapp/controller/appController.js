d1360112e1a2af2b6a572a4b2af627f31ccff914
Kansara Kinnar <kansara.k@husky.neu.edu>
2019-03-29 18:57:30 -0400

    DB conf in webapp. usd router in controller.

--- webapp/controller/appController.js
+++ webapp/controller/appController.js
@@ -1,7 +1,16 @@
-'use strict';
+const express = require('express');
+var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
+const bcrypt = require('bcrypt');
+var validator = require('validator');
+var passwordValidator = require('password-validator');
 
-module.exports = function(app) {
- app.get('/', function(req, res) {
-   res.render('../view/index');
- });
-};
+// var {User} = require('../model/user.js');
+var User = require('../model/commonModel');
+
+router.get('/',(req, res) =>{
+    res.render('../view/index');
+});
+
+
+module.exports = router;
--- webapp/index.js
+++ webapp/index.js
@@ -6,21 +6,24 @@ var express = require('express');
 const bodyParser = require('body-parser');
 // const cors = require('cors');
 
+const {mongoose} = require('./db.js');
 var routes = require('./controller/appController');
 var port = process.env.PORT || 3000;
 var app = express();
+app.use(bodyParser.json());
 // ================================================================
 // setup our express application
 // ================================================================
-app.use('/webapp', express.static(process.cwd() + '/'));
+// app.use('/webapp', express.static(process.cwd() + '/'));
+app.use('/', routes);
 app.set('view engine', 'ejs');
 // ================================================================
 // setup routes
 // ================================================================
-routes(app);
+// routes(app);
 // ================================================================
 // start our server
 // ================================================================
 app.listen(port, function() {
- console.log('Server listening on port ' + port + '…');
+ console.log('Server listening on port ' + port + '...');
 });
