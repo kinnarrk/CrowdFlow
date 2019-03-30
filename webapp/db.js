ac2a59783243d637c7ff1d018acaa188deaf21d2
Kansara Kinnar <kansara.k@husky.neu.edu>
2019-03-30 00:39:48 -0400

    Updated the live db url with credentials

--- webapp/db.js
+++ webapp/db.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CrowdFlow',
+// mongodb+srv://admin:Pandavas12345@cluster0-3rsdu.mongodb.net/test?retryWrites=true
+
+mongoose.connect('mongodb+srv://admin:Pandavas12345@cluster0-3rsdu.mongodb.net/CrowdFlow?retryWrites=true',
     {useNewUrlParser: true}, 
     (err)=>{
         if(!err)
