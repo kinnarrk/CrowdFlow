c24d4081b21eb4b993a491b42e5480465feec458
burugupalli.d <burugupalli.d@husky.neu.edu>
2019-03-30 01:51:39 +0000

    appController.js edited online with Bitbucket

--- webapp/controller/appController.js
+++ webapp/controller/appController.js
@@ -1,6 +1,16 @@
-'use strict';
-module.exports = function(app) {
- app.get('/', function(req, res) {
-   res.render('../view/index');
- });
-};
+const express = require('express');
+var router = express.Router();
+var ObjectId = require('mongoose').Types.ObjectId;
+const bcrypt = require('bcrypt');
+var validator = require('validator');
+var passwordValidator = require('password-validator');
+
+// var {User} = require('../model/user.js');
+var User = require('../model/commonModel');
+
+router.get('/',(req, res) =>{
+    res.render('../view/index');
+});
+
+
+module.exports = router;
