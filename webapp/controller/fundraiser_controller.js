833a09229c82bb6b8b9088841866ffa2c45b7526
husky_Vivek_Shakya_2789 <shakya.v@husky.neu.edu>
2019-04-15 19:12:17 -0400

    View_fundraiser

--- webapp/controller/fundraiser_controller.js
+++ webapp/controller/fundraiser_controller.js
@@ -7,6 +7,10 @@ const fundraiser = model.Fundraiser;
 const Cause = model.Cause;
 const {ensureAuthenticated } = require('../config/auth');
 
+// following lines added by Vivek on 15 april
+const Comment = model.Comment;
+var commentList = [];
+//
 // var {User} = require('../model/user.js');
 router.get('/cause', (req, res) => {
     Cause.find({}, (err, arr) => {
@@ -24,39 +28,81 @@ router.get('/start/:id', (req, res) => {
     res.render('../view/start_fundraiser');
 });
 
-router.get('/:id',(req, res) =>{
-    fundraiser.findById({"_id":req.params.id},(err,event)=>{
-        console.log(event);
-        if(err){
-            res.render({'messages':err});
-        }
-        else{
-            res.render('../view/view_fundraiser',{event:event});
-        }
-    })
-    
-});
-router.post('/:id/comment',(req, res) =>{
-    fundraiser.findById({"_id":req.params.id},(err,event)=>{
-        console.log(event);
-        if(err){
-            res.render({'messages':err});
-        }
-        else{
+// following code edited by Vivek on 15th april
 
-            const newComment = new Comment({
-                comment:req.body.comment
+router.get('/:id', (req, res) => {
+    fundraiser.findById({ "_id": req.params.id }, (err, event) => {
+        console.log(event);
+        image1 = "../view/images/fundraiser_1.jpg";// event.image.replace(/\\/g, "/");
+        console.log(image1);
+        if (err) {
+            res.render({ 'messages': err });
+
+        }
+        else {
+
+            Comment.find({ fundraiserId: req.params.id }, (err, comments) => {
+                //console.log(comments);
+                if (err) {
+                    res.render({ 'messages': err });
+                }
+                else {
+                   
+                            commentList = comments;
+
+                            console.log(commentList);
+                            console.log(image1);
+                            //console.log(userList);
+                            res.render('../view/view_fundraiser', { event: event, image1 : image1,commentList: commentList});
+                        }
+                    })
+               
+              // console.log(userList);
+
+           
+                    //console.log(comments);
+                    // commentList = comments;
+
+                    // console.log(commentList);
+                    // console.log(image1);
+                    // console.log(userList);
+                   // res.render('../view/view_fundraiser', { event: event, image1, commentList,userList,DonorList });
+                }
+
+
+
             });
-            console.log(req.body.comment);
 
 
-            newComment.save();
-            
-            res.render('../view/view_fundraiser',{event:event});
-        }
+
+    });
+//
+
+// following code edited by Vivek on 15th april
+router.post('/fundraiser_comment/:id/comment', (req, res) => {
+    const newComment = new Comment({
+        comment: req.body.comment,
+        fundraiserId: req.params.id,
+        createdBy: "5ca2fbd72d28de361ef43b76"
     })
-    
+    console.log(newComment);
+
+    newComment.save().then(user => {
+        // req.flash(
+        //     'success_msg',
+        //     'You are now registered and can log in'
+        // );
+        errors = [];
+
+        res.redirect('/fundraiser/' + req.params.id);
+
+    })
+        .catch(err => console.log(err));
+
+
+
 });
+
 
 
 
